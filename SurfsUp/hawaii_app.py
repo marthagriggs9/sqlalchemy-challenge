b8fbb490f4daf0f417b1e4368d0a8052a50a9b94
Martha <martha.griggs9@gmail.com>
2023-03-05 13:37:12 -0600

    updates

---
+++ SurfsUp/hawaii_app.py
@@ -0,0 +1,38 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine(r"sqlite:///C:\Users\Martha\GitHubClones\sqlalchemy-challenge\Resources\hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#session = Session(engine)
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
